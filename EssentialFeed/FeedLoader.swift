4d34e055bb67ab6875c1ffaaa669ce2c6cba1b0d
Cronay <inf@yannic-borgfeld.de>
2020-08-07 11:53:08 +0200

    Create the FeedLoader Protocol boundary

---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,18 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Cronay on 07.08.20.
+//  Copyright © 2020 Yannic Borgfeld. All rights reserved.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
