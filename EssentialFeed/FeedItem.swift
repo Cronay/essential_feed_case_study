1e8c3f29689e3ce9c158fa2309a21d844bb07255
Cronay <inf@yannic-borgfeld.de>
2020-08-07 11:45:49 +0200

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Cronay on 07.08.20.
+//  Copyright © 2020 Yannic Borgfeld. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let ui: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
