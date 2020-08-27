450342b40e3e5b16fd0ee77c6f3ab8e9978801c6
Cronay <inf@yannic-borgfeld.de>
2020-08-27 14:27:40 +0200

    Save command requests cache deletion

--- EssentialFeed/EssentialFeedTests/FeedCache/CacheFeedUseCase.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/CacheFeedUseCase.swift
@@ -7,15 +7,26 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class LocalFeedLoader {
-    init(store: FeedStore) {
+    let store: FeedStore
 
+    init(store: FeedStore) {
+        self.store = store
+    }
+
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
 class FeedStore {
     var deleteCacheFeedCallCount = 0
+
+    func deleteCachedFeed() {
+        deleteCacheFeedCallCount += 1
+    }
 }
 
 class CacheFeedUseCase: XCTestCase {
@@ -24,5 +35,25 @@ class CacheFeedUseCase: XCTestCase {
         _ = LocalFeedLoader(store: store)
 
         XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
+    }
+
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+
+        sut.save(items)
+
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
+    }
+
+    // MARK: - Helpers
+
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+
+    private func anyURL() -> URL {
+        return URL(string: "http://a-url.com")!
     }
 }
