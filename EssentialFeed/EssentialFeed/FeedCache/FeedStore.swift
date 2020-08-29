a21e0cf42c329357693419d417d8c649b901056f
Cronay <inf@yannic-borgfeld.de>
2020-08-29 17:43:45 +0200

    Move `FeedStore` to its own file

---
+++ EssentialFeed/EssentialFeed/FeedCache/FeedStore.swift
@@ -0,0 +1,17 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Cronay on 29.08.20.
+//  Copyright © 2020 Yannic Borgfeld. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
