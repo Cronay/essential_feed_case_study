aaca2b9c9abea359bfc42437555047fc6d8a8fbc
Cronay <inf@yannic-borgfeld.de>
2020-09-16 19:00:23 +0200

    Move `FeedCachePolicy` to its own file

---
+++ EssentialFeed/EssentialFeed/FeedCache/FeedCachePolicy.swift
@@ -0,0 +1,26 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Cronay on 16.09.20.
+//  Copyright © 2020 Yannic Borgfeld. All rights reserved.
+//
+
+import Foundation
+
+internal final class FeedCachePolicy {
+    private init() {}
+
+    static private let calendar = Calendar(identifier: .gregorian)
+
+    static private var maxCacheAgeInDays: Int {
+        return 7
+    }
+
+    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+}
