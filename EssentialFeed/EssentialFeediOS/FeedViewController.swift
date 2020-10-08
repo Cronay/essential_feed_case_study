db61538e9d6464dee03a540dd533f388468d61da
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-10-08 17:03:41 +0200

    Move `FeedViewController` to production target

---
+++ EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FeedViewController.swift
+//  EssentialFeediOS
+//
+//  Created by Cronay on 08.10.20.
+//  Copyright © 2020 Yannic Borgfeld. All rights reserved.
+//
+
+import UIKit
+import EssentialFeed
+
+public final class FeedViewController: UITableViewController {
+    private var loader: FeedLoader?
+
+    public convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        load()
+    }
+
+    @objc private func load() {
+        refreshControl?.beginRefreshing()
+        loader?.load() { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
+    }
+}
