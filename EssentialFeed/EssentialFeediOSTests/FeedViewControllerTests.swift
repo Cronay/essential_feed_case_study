a019ed04c6e88920cfdd7eb4d7fc76042e57cf6f
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-10-10 19:53:31 +0200

    Removed unnecessary selfs in extension

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -345,8 +345,8 @@ private extension FeedImageCell {
 
 private extension UIRefreshControl {
     func simulatePullToRefresh() {
-        self.allTargets.forEach { target in
-            self.actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
                 (target as NSObject).perform(Selector($0))
             }
         }
