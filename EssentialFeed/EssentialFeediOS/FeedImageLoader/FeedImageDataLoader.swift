dbdf2648a3fe7a65238c7748ffed499b4f9573c2
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-10-12 17:32:11 +0200

    Introduced group structure to separate the feed feature according to the MVC pattern

---
+++ EssentialFeed/EssentialFeediOS/FeedImageLoader/FeedImageDataLoader.swift
@@ -0,0 +1,19 @@
+//
+//  FeedImageDataLoader.swift
+//  EssentialFeediOS
+//
+//  Created by Cronay on 12.10.20.
+//  Copyright © 2020 Yannic Borgfeld. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
