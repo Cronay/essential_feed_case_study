aacac527aa32c4343a6414cc89cc1e0b5994294e
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-10-18 16:53:56 +0200

    Move View Models to their own files

---
+++ EssentialFeed/EssentialFeediOS/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Cronay on 18.10.20.
+//  Copyright © 2020 Yannic Borgfeld. All rights reserved.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
