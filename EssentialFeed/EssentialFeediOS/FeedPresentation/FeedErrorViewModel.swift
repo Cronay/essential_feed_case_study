5c7d706641b9924f084543a9fc9e86293512d962
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-10-31 13:26:43 +0100

    Move feed error view model to separate file

---
+++ EssentialFeed/EssentialFeediOS/FeedPresentation/FeedErrorViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Cronay on 31.10.20.
+//  Copyright © 2020 Yannic Borgfeld. All rights reserved.
+//
+
+struct FeedErrorViewModel {
+    let message: String?
+}
