06b8310a0b725d9ecb78f052e982a1f0f54bc060
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-10-31 17:45:44 +0100

    Move `FeedViewModel` into separate file

---
+++ EssentialFeed/EssentialFeed/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Cronay on 31.10.20.
+//  Copyright © 2020 Yannic Borgfeld. All rights reserved.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
