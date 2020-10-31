73e0cc23090aabe344306fee06bb478982886454
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-10-31 12:57:07 +0100

    Move ErrorView to separate file

---
+++ EssentialFeed/EssentialFeediOS/FeedUI/Views/ErrorView.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Cronay on 31.10.20.
+//  Copyright © 2020 Yannic Borgfeld. All rights reserved.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
