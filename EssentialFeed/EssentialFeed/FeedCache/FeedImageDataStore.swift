fd16467a4911d859aed359ff86c712240fb808e2
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-11-06 21:55:57 +0100

    Move `FeedImageDataStore` and `LocalFeedImageDataLoader` to production

---
+++ EssentialFeed/EssentialFeed/FeedCache/FeedImageDataStore.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageDataStore.swift
+//  EssentialFeed
+//
+//  Created by Cronay on 06.11.20.
+//  Copyright © 2020 Yannic Borgfeld. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedImageDataStore {
+    typealias Result = Swift.Result<Data?, Error>
+
+    func retrieve(dataForURL url: URL, completion: @escaping (Result) -> Void)
+}
