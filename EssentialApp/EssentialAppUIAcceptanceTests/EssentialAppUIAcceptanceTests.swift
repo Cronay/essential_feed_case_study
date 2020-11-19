6710ad5b8a1ed2a73c0d9cf9b259fedea85968dd
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-11-19 16:25:18 +0100

    Improve coverage by using identifiers and assuring that cached feed is displayed on launch when customer has no connectivity

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -13,8 +13,26 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
         let app = XCUIApplication()
 
         app.launch()
+        let feedCells = app.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(feedCells.count, 22)
 
-        XCTAssertEqual(app.cells.count, 22)
-        XCTAssertEqual(app.cells.firstMatch.images.count, 1)
+        let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
+        XCTAssertTrue(firstImage.exists)
+    }
+
+    func test_onLaunch_displaysCacheRemoteFeedWhenCustomerHasNoConnectivity() {
+        let onlineApp = XCUIApplication()
+        onlineApp.launch()
+
+        let offlineApp = XCUIApplication()
+        offlineApp.launchArguments = ["-connectivity", "offline"]
+        offlineApp.launch()
+
+        let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(cachedFeedCells.count, 22)
+
+        let cachedFirstImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
+        XCTAssertTrue(cachedFirstImage.exists)
+
     }
 }
