30d26ab798af378cca091fc7314d3a2de37e7050
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-11-29 17:51:04 +0100

    Record feed with failed image loading snapshot

--- EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -36,6 +36,16 @@ class FeedSnapshotTests: XCTestCase {
         record(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
     }
 
+    func test_feedWithFailedImageLoading() {
+        let sut = makeSUT()
+
+        sut.display(feedWithFailedImageLoading())
+
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_IMAGE_LOADING")
+    }
+
+    
+
     // MARK: - Helpers
 
     private func makeSUT() -> FeedViewController {
@@ -51,19 +61,27 @@ class FeedSnapshotTests: XCTestCase {
     }
 
     private func feedWithContent() -> [ImageStub] {
-            return [
-                ImageStub(
-                    description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
-                    location: "East Side Gallery\nMemorial in Berlin, Germany",
-                    image: UIImage.make(withColor: .red)
-                ),
-                ImageStub(
-                    description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
-                    location: "Garth Pier",
-                    image: UIImage.make(withColor: .green)
-                )
-            ]
-        }
+        return [
+            ImageStub(
+                description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                location: "East Side Gallery\nMemorial in Berlin, Germany",
+                image: UIImage.make(withColor: .red)
+            ),
+            ImageStub(
+                description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
+                location: "Garth Pier",
+                image: UIImage.make(withColor: .green)
+            )
+        ]
+    }
+
+    private func feedWithFailedImageLoading() -> [ImageStub] {
+        return [
+            ImageStub(description: nil, location: "Cannon Street, London", image: nil),
+            ImageStub(description: nil, location: "Brigthon Seafront", image: nil)
+        ]
+    }
+
     private func record(snapshot: UIImage, named name: String, file: StaticString = #filePath, line: UInt = #line) {
         guard let snapshotData = snapshot.pngData() else {
             return XCTFail("Failed to generate PNG data representation from snapshot", file: file, line: line)
