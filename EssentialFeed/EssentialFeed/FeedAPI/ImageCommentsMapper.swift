cbda5a826c6142d26fecb6712e7824218e2526e9
Cronay <inf@yannic-borgfeld.de>
2022-01-26 18:33:39 +0100

    Duplicated code for image comments loader

---
+++ EssentialFeed/EssentialFeed/FeedAPI/ImageCommentsMapper.swift
@@ -0,0 +1,25 @@
+//
+//  ImageCommentsMapper.swift
+//  EssentialFeed
+//
+//  Created by Yannic Borgfeld on 26.01.22.
+//  Copyright © 2022 Yannic Borgfeld. All rights reserved.
+//
+
+import Foundation
+
+class ImageCommentsMapper {
+    private struct Root: Decodable {
+        let items: [RemoteFeedItem]
+    }
+
+    private static var OK_200: Int { return 200 }
+
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+        guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            throw RemoteImageCommentsLoader.Error.invalidData
+        }
+
+        return root.items
+    }
+}
