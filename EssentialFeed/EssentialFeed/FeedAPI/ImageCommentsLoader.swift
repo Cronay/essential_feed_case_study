7e33242343889c50e7830bd209dc494902fee63e
Cronay <inf@yannic-borgfeld.de>
2022-01-26 19:35:36 +0100

    Fix indentation

--- EssentialFeed/EssentialFeed/FeedAPI/ImageCommentsLoader.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/ImageCommentsLoader.swift
@@ -11,30 +11,30 @@ import Foundation
 public final class RemoteImageCommentsLoader {
     private let url: URL
     private let client: HTTPClient
-
+    
     public enum Error: Swift.Error {
         case connectivity
         case invalidData
     }
-
+    
     public typealias Result = Swift.Result<[ImageComment], Swift.Error>
-
+    
     public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
-
+    
     public func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { [weak self] result in
             guard self != nil else { return }
-
+            
             switch result {
-                case let .success((data, response)):
-                    completion(Result {
-                        try ImageCommentsMapper.map(data, from: response)
-                    })
-                case .failure:
-                    completion(.failure(Error.connectivity))
+            case let .success((data, response)):
+                completion(Result {
+                    try ImageCommentsMapper.map(data, from: response)
+                })
+            case .failure:
+                completion(.failure(Error.connectivity))
             }
         }
     }
