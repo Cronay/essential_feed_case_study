2f96d8f6033178f8d06ab216e757ac101a4c316a
Cronay <inf@yannic-borgfeld.de>
2022-01-29 14:26:03 +0100

    Add `FeedPresenter.map`

--- EssentialFeed/EssentialFeedTests/FeedPresentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedPresentation/FeedPresenterTests.swift
@@ -10,6 +10,14 @@ import XCTest
 import EssentialFeed
 
 class FeedPresenterTests: XCTestCase {
+    
+    func test_map_createsViewModel() {
+        let feed = uniqueImageFeed().models
+        
+        let viewModel = FeedPresenter.map(feed)
+        
+        XCTAssertEqual(viewModel.feed, feed)
+    }
 
     func test_title_isLocalized() {
         XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
