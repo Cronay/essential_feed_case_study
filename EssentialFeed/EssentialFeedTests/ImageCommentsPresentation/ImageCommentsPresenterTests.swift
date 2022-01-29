e303efe75e3f8bfe969bbe809e1e8ee673c6178c
Cronay <inf@yannic-borgfeld.de>
2022-01-29 16:52:39 +0100

    Add mapping from `ImageComment` to `ImageCommentViewModel`

--- EssentialFeed/EssentialFeedTests/ImageCommentsPresentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/ImageCommentsPresentation/ImageCommentsPresenterTests.swift
@@ -14,6 +14,47 @@ class ImageCommentsPresenterTests: XCTestCase {
         XCTAssertEqual(ImageCommentsPresenter.title, localized("IMAGE_COMMENTS_VIEW_TITLE"))
     }
     
+    func test_map_createsViewModels() {
+        let now = Date()
+        let calendar = Calendar(identifier: .gregorian)
+        let locale = Locale(identifier: "en_US_POSIX")
+        
+        let comments = [
+            ImageComment(
+                id: UUID(),
+                message: "a message",
+                createdAt: now.adding(minutes: -5),
+                author: "a username"
+            ),
+            ImageComment(
+                id: UUID(),
+                message: "another message",
+                createdAt: now.adding(days: -1),
+                author: "another username"
+            )
+        ]
+        
+        let viewModel = ImageCommentsPresenter.map(
+            comments,
+            currentDate: now,
+            calendar: calendar,
+            locale: locale
+        )
+        
+        XCTAssertEqual(viewModel.comments, [
+            ImageCommentViewModel(
+                message: "a message",
+                createdAt: "5 minutes ago",
+                author: "a username"
+            ),
+            ImageCommentViewModel(
+                message: "another message",
+                createdAt: "1 day ago",
+                author: "another username"
+            )
+        ])
+    }
+    
     // MARK: - Helpers
 
     private func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
