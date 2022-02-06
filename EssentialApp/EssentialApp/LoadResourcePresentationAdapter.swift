96eb6b05bed486be97cb95d99dcdbf2334c946ae
Cronay <inf@yannic-borgfeld.de>
2022-02-06 18:11:42 +0100

    Proves request is cancelled on sut deinit

--- EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
+++ EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
@@ -12,7 +12,7 @@ import EssentialFeediOS
 
 final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     private let loader: () -> AnyPublisher<Resource, Error>
-    private var cancellable: Cancellable?
+    private var cancellable: AnyCancellable?
     var presenter: LoadResourcePresenter<Resource, View>?
 
     init(loader: @escaping () -> AnyPublisher<Resource, Error>) {
