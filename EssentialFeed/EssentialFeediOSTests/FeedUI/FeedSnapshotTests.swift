b1f66ad998da75d967ef7a5f0a96f3c6fac27012
Cronay <inf@yannic-borgfeld.de>
2022-03-28 18:17:49 +0200

    Add "Load More" cell controller to show load more spinner

--- EssentialFeed/EssentialFeediOSTests/FeedUI/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedUI/FeedSnapshotTests.swift
@@ -30,6 +30,16 @@ class FeedSnapshotTests: XCTestCase, SnapshotTestCase {
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_FAILED_IMAGE_LOADING_light")
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_FAILED_IMAGE_LOADING_dark")
     }
+    
+    func test_feedWithLoadMoreIndicator() {
+        let sut = makeSUT()
+
+        sut.display(feedWithLoadMoreIndicator())
+
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_LOAD_MORE_INDICATOR_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_LOAD_MORE_INDICATOR_dark")
+    }
+
 
     // MARK: - Helpers
 
@@ -62,6 +72,21 @@ class FeedSnapshotTests: XCTestCase, SnapshotTestCase {
         return [
             ImageStub(description: nil, location: "Cannon Street, London", image: nil),
             ImageStub(description: nil, location: "Brigthon Seafront", image: nil)
+        ]
+    }
+    
+    private func feedWithLoadMoreIndicator() -> [CellController] {
+        let stub = feedWithContent().last!
+        let cellController = FeedImageCellController(viewModel: stub.viewModel,
+                                                     delegate: stub,
+                                                     selection: {})
+        stub.controller = cellController
+        
+        let loadMore = LoadMoreCellController()
+        loadMore.display(ResourceLoadingViewModel(isLoading: true))
+        return [
+            CellController(id: UUID(), cellController),
+            CellController(id: UUID(), loadMore)
         ]
     }
 }
