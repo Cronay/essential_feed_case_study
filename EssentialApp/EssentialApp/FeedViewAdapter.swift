c5b7ae79b4492c8296ae7e44fecb11e749f64d6c
Cronay <inf@yannic-borgfeld.de>
2022-03-28 19:22:42 +0200

    Load more items on will display load more cell

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -24,7 +24,7 @@ final class FeedViewAdapter: ResourceView {
     }
 
     func display(_ viewModel: Paginated<FeedImage>) {
-        controller?.display(viewModel.items.map { model in
+        let feed: [CellController] = viewModel.items.map { model in
             let adapter = LoadResourcePresentationAdapter<Data, WeakRefVirtualProxy<FeedImageCellController>>(loader: { [imageLoader] in
                 imageLoader(model.url)
             })
@@ -36,7 +36,7 @@ final class FeedViewAdapter: ResourceView {
                     self?.selection(model)
                 }
             )
-
+            
             adapter.presenter = LoadResourcePresenter(
                 resourceView: WeakRefVirtualProxy(view),
                 loadingView: WeakRefVirtualProxy(view),
@@ -47,9 +47,15 @@ final class FeedViewAdapter: ResourceView {
                     }
                     return image
                 })
-
+            
             return CellController(id: model, view)
-        })
+        }
+        
+        let loadMore = LoadMoreCellController(callback: { viewModel.loadMore?({ _ in }) })
+        
+        let loadMoreSection = [CellController(id: UUID(), loadMore)]
+        
+        controller?.display(feed, loadMoreSection)
     }
 }
 
--- EssentialApp/EssentialAppTests/FeedUI/Helper/ListViewController+FeedTestHelpers.swift
+++ EssentialApp/EssentialAppTests/FeedUI/Helper/ListViewController+FeedTestHelpers.swift
@@ -38,6 +38,13 @@ extension ListViewController {
         let index = IndexPath(row: row, section: feedImagesSection)
         ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
     }
+    
+    func simulateLoadMoreFeedAction() {
+        guard let view = cell(row: 0, section: feedLoadMoreSection) else { return }
+        let delegate = tableView.delegate
+        let index = IndexPath(row: 0, section: feedLoadMoreSection)
+        delegate?.tableView?(tableView, willDisplay: view, forRowAt: index)
+    }
 
     func renderedFeedImageData(at index: Int) -> Data? {
         return simulateFeedImageViewVisible(at: index)?.renderedImage
@@ -49,6 +56,10 @@ extension ListViewController {
 
     private var feedImagesSection: Int {
         return 0
+    }
+    
+    private var feedLoadMoreSection: Int {
+        return 1
     }
 
     func feedImageView(at row: Int) -> UITableViewCell? {
